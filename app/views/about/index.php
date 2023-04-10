d581397f39c52314653cabb7a4bafde3469ad49c
ahdianmirza <ahdianmirza@gmail.com>
2023-04-11 00:08:23 +0700

    menambahkan file latihan assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>. Umur saya <?= $data['umur']; ?> tahun. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img class="rounded-circle shadow" src="<?= BASEURL; ?>/img/ahdian.jpg" alt="Ahdian Mirza">
+    <p class="mt-2">Halo, nama saya <?= $data['nama']; ?>. Umur saya <?= $data['umur']; ?> tahun. Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
